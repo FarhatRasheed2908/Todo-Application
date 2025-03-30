fdfcadbe31953ed0e7aec2c1db10fe89331a1e86
Farhat Rasheed <farhat3822@gmail.com>
2025-03-30 00:16:45 -0700

    Complete

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var getul = document.getElementById('list')
-//var id = document.getElementById('para')
-//var totalPrice = 0;
+
 function addbtn(){
  var a = document.getElementById('input')
-   // var b = document.getElementById('inp')
-    //var price = parseFloat(b.value);
-    //if ( price <= 0) {
-      //alert('Please enter a valid price.');
-      //return;
-  //}
+  
     var li = document.createElement('li')
     var btn = document.createElement('button')
     var del = document.createTextNode('Delete')
@@ -25,41 +19,20 @@ function addbtn(){
     editbtn.setAttribute('onclick','editfun(this)')
       a.value = ''
       b.value = ''
-      //totalPrice += price;
-      //updateTotal();
-     }
+    
 function deleteall(){
   getul.innerHTML = ''
-  //totalPrice = 0;
-  //updateTotal();
+
 
 }
 function dellist(e){
-  //var newli = e.parentNode.firstChild.nodeValue.trim();
-  //var price = parseFloat(newli.split('$')[1]);
- // totalPrice -= price;
+
 e.parentNode.remove()
-//updateTotal();
 }
 function editfun(e){
  var a = prompt('Enter edit value',e.parentNode.firstChild.nodeValue)
  e.parentNode.firstChild.nodeValue = a
-    // var existingValue = e.parentNode.firstChild.nodeValue.trim();
-  //var [item, priceText] = existingValue.split('$');
-  //var price = parseFloat(priceText);
-  //totalPrice = totalPrice -price + price1;
-  //var newli = e.parentNode.firstChild.nodeValue.trim();
-//var create = newli.split(' ');
-//var item = create[0]
-//var price1 = create[1]
 var userEdit = prompt('Enter edit value',item)
   item = userEdit  
-// var user = +prompt('Enter corrrect Price',price1)
-//price1 = user
 e.parentNode.firstChild.nodeValue = `${item} `
-//updateTotal();
-}
-function updateTotal(){
-  //id.textContent = `Total Price: $${totalPrice}`;
-
 }
